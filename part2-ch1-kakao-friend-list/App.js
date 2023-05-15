4df92039b978833128021cf307b7cffac3a1a423
Joe <nastillmatic@gmail.com>
2023-05-15 13:27:33 +0900

    Part2. Ch01-05-UI 개발 (3) 친구 리스트, 내 프로필 컴포넌트 재사용하기

--- part2-ch1-kakao-friend-list/App.js
+++ part2-ch1-kakao-friend-list/App.js
@@ -2,22 +2,44 @@ import { StatusBar } from "expo-status-bar";
 import { Platform, StyleSheet, Text, View } from "react-native";
 import Header from "./src/Header";
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import MyProfile from "./src/MyProfile";
-import { myProfile } from "./src/data";
+import Profile from "./src/Profile";
+import { friendProfiles, myProfile } from "./src/data";
 import Margin from "./src/Margin";
+import Division from "./src/Division";
+import FriendSection from "./src/FriendSection";
+import FriendList from "./src/FriendList";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+const statusBarHeight = getStatusBarHeight(true);
 
 export default function App() {
+
+  const onPressArrow = () => {
+    console.log('clicked arrow');
+  }
+
   return (
     <SafeAreaProvider>      
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>        
         <Header />
 
         <Margin height={10} />
  
-        <MyProfile
+        <Profile
           uri={myProfile.uri}
           name={myProfile.name}
           introduction={myProfile.introduction}
+        />
+
+        <Margin height={15} />
+        <Division /> 
+        <Margin height={12} />
+        <FriendSection 
+          friendProfileLen={friendProfiles.length}
+          onPressArrow={onPressArrow}
+        />
+        <FriendList
+          data={friendProfiles}
         />
       </SafeAreaView>
     </SafeAreaProvider>
@@ -28,5 +50,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    paddingTop: statusBarHeight,
+    paddingHorizontal: 15,
   },
 })
---
+++ part2-ch1-kakao-friend-list/src/FriendList.js
@@ -0,0 +1,25 @@
+import { ScrollView, View } from "react-native";
+import Profile from "./Profile";
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { getBottomSpace } from "react-native-iphone-x-helper";
+import Margin from "./Margin";
+
+const bottomSpace = getBottomSpace(); // not working
+
+export default (props) => {
+    const insets = useSafeAreaInsets();
+    return (                
+        <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: bottomSpace }}>
+            {props.data.map((item, index) => (
+                <View key={index}>                    
+                    <Profile                    
+                    uri={item.uri}
+                    name={item.name}
+                    introduction={item.introduction}
+                    />
+                    <Margin height={13} />
+                </View>                
+            ))}
+        </ScrollView>
+    )
+};
