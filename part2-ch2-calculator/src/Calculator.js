9d06df0d8f53690df4fb089cc58f12dca4c94ef5
Joe <nastillmatic@gmail.com>
2023-05-17 16:24:35 +0900

    Part2. Ch02-04-UI 개발 (2) 계산 결과 텍스트

--- part2-ch2-calculator/src/Calculator.js
+++ part2-ch2-calculator/src/Calculator.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TouchableOpacity, View, Text } from 'react-native';
 import styled from 'styled-components/native';
-
 
 // Button type: 'reset' | 'operator' | 'num'
 const Button = ({ text, onPress, flex, type }) => {
@@ -14,7 +13,7 @@ const Button = ({ text, onPress, flex, type }) => {
   return (
     <TouchableOpacity 
     onPress={onPress} 
-    style={{ 
+    style={{
         flex,
         backgroundColor,
         justifyContent: "center",
@@ -40,125 +39,143 @@ const ButtonContainer = styled.View`
     width: 100%;
 `;
 
+const InputContainer = styled.View`
+    background-color: ${COLOR.RESULT};
+    min-height: 50px;
+    justify-content: center;
+    align-items: flex-end;
+    /* padding: 5px; // top, right, bottom, left */
+    padding: 10px 5px; // top,bottom(vertical), left,right(horizontal)
+    /* padding: 1px 2px 3px 4px; // top right botoom left */
+`;
 
 export default () => {
-  return (
-    <View style={{ flex: 1, width: "250" }}>
-    {/* 결과 */}
 
-    {/* [AC ~ /] */}
-        <ButtonContainer>      
-            <Button
-                type="reset"
-                text="AC"
-                onPress={() => null}
-                flex={3}
-            />
-            <Button
-                type="operator"
-                text="/"
-                onPress={() => null}
-                flex={1}
-            />
-        </ButtonContainer>
+    const [input, setInput] = useState(0);
+    const [currentOperator, setCurrentOperator] = useState(null);
+    const [result, setResult] = useState(null);
+    const [tempInput, setTempInput] = useState(null);
+    const [tempOperator, setTempOperator] = useState(null);
+  
+    return (
+        <View style={{ flex: 1, width: 250, justifyContent: "center" }}>
+            {/* 결과 */}
+            <InputContainer>
+                <Text style={{ color: "white", fontSize: 35, textAlign: "right" }}>{input}</Text>
+            </InputContainer>
+            {/* [AC ~ /] */}
+            <ButtonContainer>      
+                <Button
+                    type="reset"
+                    text="AC"
+                    onPress={() => null}
+                    flex={3}
+                />
+                <Button
+                    type="operator"
+                    text="/"
+                    onPress={() => null}
+                    flex={1}
+                />
+            </ButtonContainer>
 
-    {/* [7 ~ x] */}
-        <ButtonContainer>      
-            <Button
-                type="num"
-                text="7"
-                onPress={() => null}
-                flex={1}
-            />
-            <Button
-                type="num"
-                text="8"
-                onPress={() => null}
-                flex={1}
-            />
-            <Button
-                type="num"
-                text="9"
-                onPress={() => null}
-                flex={1}
-            />
-            <Button
-                type="operator"
-                text="X"
-                onPress={() => null}
-                flex={1}
-            />
-        </ButtonContainer>     
+            {/* [7 ~ x] */}
+            <ButtonContainer>      
+                <Button
+                    type="num"
+                    text="7"
+                    onPress={() => null}
+                    flex={1}
+                />
+                <Button
+                    type="num"
+                    text="8"
+                    onPress={() => null}
+                    flex={1}
+                />
+                <Button
+                    type="num"
+                    text="9"
+                    onPress={() => null}
+                    flex={1}
+                />
+                <Button
+                    type="operator"
+                    text="X"
+                    onPress={() => null}
+                    flex={1}
+                />
+            </ButtonContainer>     
 
-    {/* [4 ~ -] */}
-        <ButtonContainer>
-            <Button
-                type="num"
-                text="4"
-                onPress={() => null}
-                flex={1}
-            />
-            <Button
-                type="num"
-                text="5"
-                onPress={() => null}
-                flex={1}
-            />
-            <Button
-                type="num"
-                text="6"
-                onPress={() => null}
-                flex={1}
-            />
-            <Button
-                type="operator"
-                text="-"
-                onPress={() => null}
-                flex={1}
-            />
-        </ButtonContainer>
-    {/* [1 ~ +] */}
-        <ButtonContainer>
-            <Button
-                type="num"
-                text="1"
-                onPress={() => null}
-                flex={1}
-            />
-            <Button
-                type="num"
-                text="2"
-                onPress={() => null}
-                flex={1}
-            />
-            <Button
-                type="num"
-                text="3"
-                onPress={() => null}
-                flex={1}
-            />
-            <Button
-                type="operator"
-                text="+"
-                onPress={() => null}
-                flex={1}
-            />
-        </ButtonContainer>
-    {/* [0 ~ =] */}
-        <ButtonContainer>
-            <Button
-                type="num"
-                text="0"
-                onPress={() => null}
-                flex={3}
-            />            
-            <Button
-                type="operator"
-                text="="
-                onPress={() => null}
-                flex={1}
-            />
-        </ButtonContainer>
-    </View>
-  )
+            {/* [4 ~ -] */}
+            <ButtonContainer>
+                <Button
+                    type="num"
+                    text="4"
+                    onPress={() => null}
+                    flex={1}
+                />
+                <Button
+                    type="num"
+                    text="5"
+                    onPress={() => null}
+                    flex={1}
+                />
+                <Button
+                    type="num"
+                    text="6"
+                    onPress={() => null}
+                    flex={1}
+                />
+                <Button
+                    type="operator"
+                    text="-"
+                    onPress={() => null}
+                    flex={1}
+                />
+            </ButtonContainer>
+            {/* [1 ~ +] */}
+            <ButtonContainer>
+                <Button
+                    type="num"
+                    text="1"
+                    onPress={() => null}
+                    flex={1}
+                />
+                <Button
+                    type="num"
+                    text="2"
+                    onPress={() => null}
+                    flex={1}
+                />
+                <Button
+                    type="num"
+                    text="3"
+                    onPress={() => null}
+                    flex={1}
+                />
+                <Button
+                    type="operator"
+                    text="+"
+                    onPress={() => null}
+                    flex={1}
+                />
+            </ButtonContainer>
+            {/* [0 ~ =] */}
+            <ButtonContainer>
+                <Button
+                    type="num"
+                    text="0"
+                    onPress={() => null}
+                    flex={3}
+                />            
+                <Button
+                    type="operator"
+                    text="="
+                    onPress={() => null}
+                    flex={1}
+                />
+            </ButtonContainer>
+        </View>
+    )
 }
