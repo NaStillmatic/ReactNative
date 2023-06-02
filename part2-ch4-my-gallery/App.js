a4be9d16084b7b6a7be4eab3a356f5154e9ba6b8
Joe <nastillmatic@gmail.com>
2023-06-02 19:00:28 +0900

    Part2. Ch04-02-내장 갤러리에서 사진 선택하여 추가하기 - ImagePicker

--- part2-ch4-my-gallery/App.js
+++ part2-ch4-my-gallery/App.js
@@ -1,12 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, FlatList, Image, Platform, SafeAreaView, StyleSheet, TouchableOpacity, View, Text } from 'react-native';
+import { useGallery } from './src/use-gallery';
+
+const width = Dimensions.get('screen').width;
+const columnSize = width / 3;
 
 export default function App() {
+
+  const { images, imageWithAddButton, pickImage, deleteImage } = useGallery();
+
+  const onPressOpenGallery = () => {
+    pickImage();
+  }
+  const onLongPressImage = (imageId) => deleteImage(imageId); 
+
+  const renderItem = ({ item: { id, uri }, index }) => {    
+    
+    if (id === -1) {
+      return (      
+        <TouchableOpacity
+          onPress={onPressOpenGallery}
+          style={{ 
+            width: columnSize, 
+            height: columnSize,             
+            backgroundColor: "lightgrey",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+          <Text style={{ fontWeight: "100", fontSize: 45 }}>+</Text>
+        </TouchableOpacity>
+      );  
+    }
+    return (      
+      <TouchableOpacity onLongPress={() => onLongPressImage(id)}>
+        <Image 
+          source={{ uri }} 
+          style={{ width: columnSize, height: columnSize }} 
+        />
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={ imageWithAddButton }
+        renderItem={ renderItem }
+        numColumns={3}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +56,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === "android" ? 30: 0,
   },
 });
