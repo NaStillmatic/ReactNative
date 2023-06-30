9e05af84bd2ffa310017a09d852180dcaba9a39b
Joe <nastillmatic@gmail.com>
2023-06-30 14:22:23 +0900

    Part2. Ch05-05-SectionList - 가데이터로 만드는 renderItem

--- part2-ch5-kakao-bus/App.js
+++ part2-ch5-kakao-bus/App.js
@@ -1,44 +1,35 @@
 import { FlatList, SafeAreaView, SectionList, StyleSheet, Text, View } from 'react-native';
+import BusInfo from './src/BusInfo';
+import { COLOR } from './src/color';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{ flex: 1 }}>
-        <Text style={{ fontSize: 30, fontWeight: "bold" }}>FlatList</Text>
-        <FlatList
-          data={[
-            { busNum: 146 },
-            { busNum: 360 },
-            { busNum: 740 },
-            { busNum: 3412 },
-            { busNum: 1100 },
-            { busNum: 1700 },
-          ]}
-          renderItem={({ item }) => <Text>{item.busNum}</Text>}
-          />
-      </View>
-
-      <View style={{ flex: 1 }}>
-        <Text style={{ fontSize: 30, fontWeight:"bold" }}>SectionList</Text>
-        <SectionList
-          sections={[
-            {
-              title: "간선버스",
-              data: [{ busNum: 146 }, { busNum: 360 }, { busNum: 740 }],
-            },
-            {
-              title: "지선버스",
-              data: [{ busNum: 3412 }],
-            },
-            {
-              title: "직행버스",
-              data: [{ busNum: 1100}, { busNum: 1700 }],
-            }
-          ]}
-          renderSectionHeader={({ section: { title } }) => <Text>(title)</Text>}
-          renderItem={({ item }) => <Text>{item.busNum}</Text>}
-        />
-      </View>
+    <SafeAreaView style={styles.container}>      
+      <BusInfo
+        isBookmarked={true}
+        onPressBookmark={() => {}}
+        num={146}
+        directionDescription="강남역.강남역사거리"
+        numColor={COLOR.BUS_B}
+      />
+      {/* <SectionList
+        sections={[
+          {
+            title: "간선버스",
+            data: [{ busNum: 146 }, { busNum: 360 }, { busNum: 740 }],
+          },
+          {
+            title: "지선버스",
+            data: [{ busNum: 3412 }],
+          },
+          {
+            title: "직행버스",
+            data: [{ busNum: 1100}, { busNum: 1700 }],
+          }
+        ]}
+        renderSectionHeader={({ section: { title } }) => <Text>(title)</Text>}
+        renderItem={({ item }) => <Text>{item.busNum}</Text>}
+      />     */}
     </SafeAreaView>
   );
 }
