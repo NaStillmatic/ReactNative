d3943465944415ef559419e0f3811f4f3232a29a
Joe <nastillmatic@gmail.com>
2023-07-27 11:30:21 +0900

    Part3. Ch04-03-앞서 작성한 컴포넌트들을 함수형 컴포넌트로 전환 해 봅니다 (1)

--- part3-ch3-component-example/App.js
+++ part3-ch3-component-example/App.js
@@ -3,19 +3,38 @@ import { Button, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 // import { HeaderWithoutCompound } from './src/components/HeaderWithoutCompound';
 import { Header } from './src/components/Header/Header';
+import { Typography } from './src/components/Typography';
+import { LocalImage } from './src/components/LocalImage';
+import { RemoteImage } from './src/components/RemoteImage';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <View style={{flex: 1, }}>
-        {/* <HeaderWithoutCompound title='HEADER' /> */}
-        <Header>
-          <Header.Group>
-            <Header.Icon iconName='arrow-back' />
-            <Header.Title title='HEADER' />
-          </Header.Group>
-          <Header.Icon iconName='close' /> 
-        </Header>
+      <View style={{
+        flex: 1, 
+        alignItems: 
+        'center', 
+        justifyContent: 'center'
+      }}>
+        <Typography 
+          fontSize={16} 
+          color='black'
+        >
+          TEXT
+        </Typography>
+
+        <LocalImage 
+          localAsset={require('./assets/favicon.png')} 
+          width={100} 
+          height={100} 
+        />
+
+        <RemoteImage
+            url='https://images.unsplash.com/photo-1649191629437-2dc474bc11ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1625&q=80'
+            width={200} 
+            height={200} 
+        />
+
       </View>
     </SafeAreaProvider>
   );
--- part3-ch3-component-example/src/components/RemoteImage.js
+++ part3-ch3-component-example/src/components/RemoteImage.js
@@ -1,18 +1,30 @@
 import React from "react";
 import {Image as RNImage} from 'react-native';
 
-export class RemoteImage extends React.Component {
-    render() {
-        return (
-            <RNImage
-                source={{url: this.props.url}}
-                style={[
-                    this.props.style, {
-                        width: this.props.width, 
-                        height: this.props.height
-                    }
-                ]} 
-            />
-        )
-    }
+// export class RemoteImage extends React.Component {
+//     render() {
+//         return (
+//             <RNImage
+//                 source={{url: this.props.url}}
+//                 style={[
+//                     this.props.style, {
+//                         width: this.props.width, 
+//                         height: this.props.height
+//                     }
+//                 ]} 
+//             />
+//         )
+//     }
+// }
+
+export const RemoteImage = (props) => {
+    return (
+        <RNImage
+            source={{url: props.url}}
+            style={[props.style, {
+                width: props.width,
+                height: props.height,
+            }]}
+        />
+    )
 }
