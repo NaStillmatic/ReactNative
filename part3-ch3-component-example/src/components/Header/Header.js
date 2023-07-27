f282b29988df7051a89a3241aa7a4c2fef2977d1
Joe <nastillmatic@gmail.com>
2023-07-27 14:15:25 +0900

    Part3. Ch04-03-앞서 작성한 컴포넌트들을 함수형 컴포넌트로 전환 해 봅니다 (2)

--- part3-ch3-component-example/src/components/Header/Header.js
+++ part3-ch3-component-example/src/components/Header/Header.js
@@ -1,42 +1,68 @@
 import React from 'react';
-import { View, Dimensions } from 'react-native';
-import { SafeAreaInsetsContext } from "react-native-safe-area-context";
+import { View, Dimensions, useWindowDimensions } from 'react-native';
+import { SafeAreaInsetsContext, useSafeAreaInsets } from "react-native-safe-area-context";
 import { Spacer } from '../Spacer';
 import { HeaderTitle } from './HeaderTitle';
 import { HeaderIcon } from './HeaderButton';
 import { HeaderGroup } from './HeaderGroup';
 const { width } = Dimensions.get('window');
 
-export class Header extends React.Component {
-    render() {
-        return (
-            <SafeAreaInsetsContext.Consumer>
-                {inset=>(
-                    <View style={{paddingTop: inset.top}}>
-                        <View style={{
-                            width: width,
-                            flexDirection: 'row',
-                            height: 56,
-                            borderBottomColor: 'gray',
-                            borderBottomWidth: 1,
-                            alignItems: 'center'
-                        }}>
-                            <Spacer horizontal={true} space={12} />
-                            <View style={{
-                                flex:1,
-                                flexDirection: 'row',
-                                justifyContent: 'space-between'
-                            }}>
-                                {this.props.children}
-                            </View>
-                            <Spacer horizontal={true} space={12} />
-                        </View>
-                    </View>
-                )}
-            </SafeAreaInsetsContext.Consumer>
-        )
-    }
+export const Header = (props) => {
+    const inset = useSafeAreaInsets();
+    const {width} = useWindowDimensions();
+    return (
+        <View style={{paddingTop: inset.top}}>
+            <View style={{
+                width: width,
+                flexDirection: 'row',
+                height: 56,
+                borderBottomColor: 'gray',
+                borderBottomWidth: 1,
+                alignItems: 'center',
+            }}>
+                <Spacer horizontal={true} space={12} />
+                <View style={{
+                    flex:1,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between'
+                }}>
+                    {props.children}
+                </View>
+            </View>
+        </View>
+    )
 }
+
+// export class Header extends React.Component {
+//     render() {
+//         return (
+//             <SafeAreaInsetsContext.Consumer>
+//                 {inset=>(
+//                     <View style={{paddingTop: inset.top}}>
+//                         <View style={{
+//                             width: width,
+//                             flexDirection: 'row',
+//                             height: 56,
+//                             borderBottomColor: 'gray',
+//                             borderBottomWidth: 1,
+//                             alignItems: 'center'
+//                         }}>
+//                             <Spacer horizontal={true} space={12} />
+//                             <View style={{
+//                                 flex:1,
+//                                 flexDirection: 'row',
+//                                 justifyContent: 'space-between'
+//                             }}>
+//                                 {this.props.children}
+//                             </View>
+//                             <Spacer horizontal={true} space={12} />
+//                         </View>
+//                     </View>
+//                 )}
+//             </SafeAreaInsetsContext.Consumer>
+//         )
+//     }
+// }
 
 Header.Title = HeaderTitle;
 Header.Icon = HeaderIcon;
