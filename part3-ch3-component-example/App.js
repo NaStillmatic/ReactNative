202525f82fbe8a77f0754d92448d36f8c4c56b57
Joe <nastillmatic@gmail.com>
2023-08-01 15:59:34 +0900

    Part3. Ch04-04-최적화를 위한 Memoization

--- part3-ch3-component-example/App.js
+++ part3-ch3-component-example/App.js
@@ -1,5 +1,5 @@
 
-import { Button, View } from 'react-native';
+import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 // import { HeaderWithoutCompound } from './src/components/HeaderWithoutCompound';
 import { Header } from './src/components/Header/Header';
@@ -8,20 +8,41 @@ import { LocalImage } from './src/components/LocalImage';
 import { RemoteImage } from './src/components/RemoteImage';
 import { Spacer } from './src/components/Spacer';
 import { Divider } from './src/components/Divider';
+import { HookTestComponent } from './src/components/HookTestComponent';
+import { useCallback, useState } from 'react';
+import { Button } from './src/components/Button';
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <View style={{flex: 1, }}>
-        <Header>
-        <Header.Group>
-            <Header.Icon iconName='arrow-back' />
-            <Header.Title title='HEADER'></Header.Title>
-        </Header.Group>
-        <Header.Icon iconName='close' />
-        </Header>
-        
-      </View>
-    </SafeAreaProvider>
-  );
+    const [a, setA] = useState(0);
+    const [b, setB] = useState(0);
+
+    const doSum = useCallback(() => {
+        return (a+b)        
+    }, [a, b])
+
+    return (
+        <SafeAreaProvider>
+        <View style={{flex: 1}}>
+            <Header>
+            <Header.Group>
+                <Header.Icon iconName='arrow-back' />
+                <Header.Title title='HEADER'></Header.Title>
+            </Header.Group>
+            <Header.Icon iconName='close' />
+            </Header>
+            <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+                <HookTestComponent a={a} b={b}></HookTestComponent>
+
+                <Typography>현재 callback으로 계산 된 값: {doSum()}</Typography>
+                <Button
+                    onPress={() =>{
+                        console.log('press'),
+                        setA(a+1)
+                    }}>
+                    <Typography>A더하기</Typography>
+                </Button>
+            </View>
+        </View>
+        </SafeAreaProvider>
+    );
 }
