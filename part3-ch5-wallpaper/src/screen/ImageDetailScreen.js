7a2b457e78de39d330014bc7a63a45359cc8a4fc
Joe <nastillmatic@gmail.com>
2023-08-02 18:03:10 +0900

    Part3. Ch05-02-외부 라이브러리 활용하기

--- part3-ch5-wallpaper/src/screen/ImageDetailScreen.js
+++ part3-ch5-wallpaper/src/screen/ImageDetailScreen.js
@@ -3,6 +3,11 @@ import { View, useWindowDimensions } from 'react-native';
 import { Header } from '../components/Header/Header';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { RemoteImage } from '../components/RemoteImage';
+import { Button } from '../components/Button';
+import { Typography } from '../components/Typography';
+import { Icon } from '../components/Icons';
+import * as FileSystem from 'expo-file-system';
+import * as MediaLibray from 'expo-media-library';
 
 export const ImageDetailScreen = (props) => {
     const navigation = useNavigation();
@@ -10,6 +15,42 @@ export const ImageDetailScreen = (props) => {
     const onPressBack = useCallback(() => {        
         navigation.goBack();
     }, [])
+
+    const onPressDownload = useCallback(async()=> {
+        const downloadResumable = FileSystem.createDownloadResumable(
+            route.params.url,
+            `${FileSystem.documentDirectory}${new Date().getMilliseconds()}.jpg`
+        )
+
+        try {
+            const {uri} = await downloadResumable.downloadAsync();
+            console.log('Finished Downloading to ', uri)
+
+            const permissionResult = await MediaLibray.getPermissionsAsync(true);
+            console.log(permissionResult);
+
+            if (permissionResult.status === 'denied') {
+                // 아예 못쓰는 상태.
+                return;
+            }
+            if (permissionResult.status === 'undetermined') {
+
+                const requestResult = await MediaLibray.requestPermissionsAsync();
+                console.log(requestResult);
+                if (requestResult.status === 'denied') {
+                    return;
+                }
+            }
+
+            const asset = await MediaLibray.createAssetAsync(uri);
+            const album = await MediaLibray.createAlbumAsync('MyFirstAlbum', asset, false);
+            console.log(album);
+
+        } catch(ex) {
+            
+        }
+    }, [])
+
     const {width} = useWindowDimensions();
 
     return (        
@@ -23,7 +64,15 @@ export const ImageDetailScreen = (props) => {
             <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}} >
                 <RemoteImage url={route.params.url} width={width} height={width * 1.5} />
             </View>
-        </View>
-        
+
+            <Button onPress={onPressDownload} >
+                <View style={{paddingBottom: 24, backgroundColor: 'black'}}>
+                    <View style={{height: 52, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                        <Typography color={'white'}>DOWNLOAD</Typography>
+                        <Icon name='download' size={24} color='white' />
+                    </View>
+                </View>
+            </Button>
+        </View>        
     )
 }
