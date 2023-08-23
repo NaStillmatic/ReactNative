20053ec0b56255365bca02c2b9f0c248ce368a55
Joe <nastillmatic@gmail.com>
2023-08-23 14:40:40 +0900

    Part3. Ch09-01-최초 화면 구성 (리스트, 등록화면) 2

--- part3-ch9-webpage-scrapper/src/screen/AddLinkScreen.js
+++ part3-ch9-webpage-scrapper/src/screen/AddLinkScreen.js
@@ -1,14 +1,23 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { View } from 'react-native';
 import { Header } from '../components/Header/Header';
+import { SingleLineInput } from '../components/SingleLineInput';
+import { Button } from '../components/Button';
+import { Typography } from '../components/Typography';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Spacer } from '../components/Spacer';
 
 export const AddLinkScreen = () => {
     const navigation = useNavigation();
+    const safeAreaInset = useSafeAreaInsets();
+
+    const [url, setUrl] = useState('');
 
     const onPressClose = useCallback(() => {
         navigation.goBack();
     }, [])
+
     return (
         <View style={{flex: 1}}>
             <Header>
@@ -18,6 +27,32 @@ export const AddLinkScreen = () => {
 
                 <Header.Icon iconName='close' onPress={onPressClose} />                
             </Header>
+
+            <View style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                paddingHorizontal: 24
+            }}>
+                <SingleLineInput
+                    value={url}
+                    onChangeText={setUrl}
+                    placeholder='https://example.com'
+                />
+            </View>
+
+            <Button>
+                <View style={{backgroundColor: url === '' ? 'gray' : 'black'}}>
+                    <View style={{
+                        height: 52, 
+                        alignItems: 'center', 
+                        justifyContent: 'center'
+                    }}>
+                        <Typography color='white' fontSize={18}>저장하기</Typography>                        
+                    </View>
+                    <Spacer space={safeAreaInset.bottom} />
+                </View>
+            </Button>
         </View>
     )
 }
--- part3-ch9-webpage-scrapper/src/screen/LinkListScreen.js
+++ part3-ch9-webpage-scrapper/src/screen/LinkListScreen.js
@@ -5,9 +5,12 @@ import { Header } from '../components/Header/Header';
 import { Button } from '../components/Button';
 import { Typography } from '../components/Typography';
 import { Spacer } from '../components/Spacer';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Icon } from '../components/Icons';
 
 export const LinkListScreen = () => {
     const navigation = useNavigation();
+    const safeAreaInset = useSafeAreaInsets();
 
     const onPressButon = useCallback(() => {
         navigation.navigate('LinkDetail')
@@ -32,6 +35,25 @@ export const LinkListScreen = () => {
 
                 <Button onPress={onPressAddButton}>
                     <Typography>링크 등록하기로 이동하기</Typography>
+                </Button>
+            </View>
+
+            <View style={{
+                position: 'absolute', 
+                right: 24,
+                bottom: 24 + safeAreaInset.bottom }}
+            >
+                <Button onPress={onPressAddButton}>
+                    <View style={{
+                        width: 52,
+                        height: 52,
+                        borderRadius: 26,
+                        alignItems: 'center',
+                        justifyContent: 'center', 
+                        backgroundColor: 'black' }}
+                    >
+                        <Icon name='add' color='white' size={32} />
+                    </View>                    
                 </Button>
             </View>
         </View>
