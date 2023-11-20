fc9bdae06ad90c219fce929425137ff70a4f8889
Joe <nastillmatic@gmail.com>
2023-11-20 16:13:27 +0900

    Part3. Ch11-04-authentication 적용해보기(2)

--- part3-ch11-firebase-integration/App.js
+++ part3-ch11-firebase-integration/App.js
@@ -1,11 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Image, StyleSheet, Text, View } from 'react-native';
+
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+import { useCallback, useEffect, useState } from 'react';
+import FirebaseAuth from '@react-native-firebase/auth';
+
+GoogleSignin.configure();
 
 export default function App() {
+  const [userInfo, setUserInfo] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const onPressGoogleSignin = useCallback(async() => {
+    try {
+      await GoogleSignin.hasPlayServices ({
+        showPlayServicesUpdateDialog:true
+      })
+      const userInfo = await GoogleSignin.signIn();
+      console.log('userInfo', userInfo);
+
+      const credential = FirebaseAuth.GoogleAuthProvider.credential(userInfo.idToken);
+      const result = await FirebaseAuth().signInWithCredential(credential);
+
+      console.log(result);
+
+      setUserInfo({
+        name:result.additionalUserInfo.profile.name,        
+        profileImage:result.additionalUserInfo.profile.picture,
+      })      
+    } catch(ex) {
+
+    }
+  }, [])
+
+  const getCurrentUserInfo = useCallback(async() => {
+    try {
+      setLoading(true);
+      const userInfo = await GoogleSignin.signInSilently();
+
+      const credential = FirebaseAuth.GoogleAuthProvider.credential(userInfo.idToken);
+      const result = await FirebaseAuth().signInWithCredential(credential);
+
+      setUserInfo({
+        name:result.additionalUserInfo.profile.name,        
+        profileImage:result.additionalUserInfo.profile.picture,
+      })
+      setLoading(false);
+    } catch(error) {
+      setLoading(false);
+      if (error.code === statusCodes.SIGN_IN_REQUIRED) {
+
+      } else {
+
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    getCurrentUserInfo();
+  }, [])
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      alignItems:'center',
+      justifyContent: 'center'
+    }}>
+      { loading ? (
+        <ActivityIndicator/>
+      ) : (
+        userInfo !== null ?
+        (
+          <View style={{}}>          
+            <Image source={{uri: userInfo.profileImage}} style={{width:100, height:100, borderRadius:50}}/>
+            <Text style={{fontSize:24, marginTop:20}}>{userInfo.name}</Text>
+          </View>
+        ): (
+          <GoogleSigninButton onPress={onPressGoogleSignin}/>
+        )
+      )}      
     </View>
   );
 }
